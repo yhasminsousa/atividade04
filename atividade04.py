aaf416ba2fb6f7f8fc66dc7b06a872c6d3bb5bba
palomalindinha01 <palomalindinha35@gmail.com>
2023-10-11 11:20:40 -0300

    first commit

---
+++ atividade04.py
@@ -0,0 +1,6 @@
+# Escreva um numero e veja qual o seu antecessor e seu sucessor! 
+num1 = int(input("digite um número:"))
+antecessor = (num1-1)
+print(antecessor)
+sucessor = (num1+1)
+print(sucessor)
